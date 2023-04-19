835441640d12869031a5e722b728bfa68eae8abb
Hawk Itzme <smquasim016@gmail.com>
2023-04-19 06:19:56 +0530

    Initial project by Android Studio

---
+++ app/src/main/java/com/example/networks_scanner_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.networks_scanner_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
